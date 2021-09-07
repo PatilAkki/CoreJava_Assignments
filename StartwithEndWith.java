2cbb3adcb472e30f7e430614a2a12d14db505cb3
PatilAkki <89850553+PatilAkki@users.noreply.github.com>
2021-09-07 18:20:54 +0530

    Add files via upload

---
+++ StartwithEndWith.java
@@ -0,0 +1,27 @@
+
+public class StartwithEndWith {
+
+	public static void main(String[] args) {
+		String s = "Abcd";
+		if(s.startsWith("A"))
+		{
+			System.out.println("string starts with 'A'");
+		}
+		else
+		{
+			System.out.println("string does not start with 'A'");
+		}
+		
+		if(s.endsWith("t"))
+		{
+			System.out.println("string end with 't'");
+		}
+		else
+		{
+			System.out.println("string does not ends with 't'");
+		}
+		
+
+	}
+
+}
