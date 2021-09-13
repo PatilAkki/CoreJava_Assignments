8c2cf87dfc9bda79b8136dce64b64a93d81b0d0a
PatilAkki <89850553+PatilAkki@users.noreply.github.com>
2021-09-13 17:06:07 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,10 @@
+package Assignment_9_Sept;
+import java.util.Scanner;
+public interface Calculation {
+	
+	double calculateSimpleIntrest(double Principle,double Rate,double Time);
+    double calculateCompoundIntrest(double Principle,double Rate,double Time);
+	 
+}
+
+
