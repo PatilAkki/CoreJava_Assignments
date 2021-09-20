e6172967e797f55fcdbebe1126d0eff9212479da
PatilAkki <89850553+PatilAkki@users.noreply.github.com>
2021-09-20 20:54:32 +0530

    Add files via upload

---
+++ CovertToUpparcase.java
@@ -0,0 +1,34 @@
+package Assignment_17_Sept;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Simple {
+	
+	public static void main(String[] args)
+	{
+		List<String> names = new ArrayList<>();
+		Scanner input= new Scanner(System.in);
+		
+		System.out.println("Enter any five names");
+		
+		for(int i=0;i<6;i++)
+		{
+			String name= input.nextLine();
+			names.add(name);
+			
+		}
+		System.out.println("Input given by user");
+		System.out.println(names);
+		
+		String[] nameArray = new String[names.size()];
+		names.toArray(nameArray);
+		
+		for(String x: nameArray)
+		{
+			System.out.println(x.toUpperCase());
+			
+		}
+	}
+
+}
